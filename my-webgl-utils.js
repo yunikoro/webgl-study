53592e362f38287d47455a1812c9d8d2c8c7cb6f
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-07-04 20:57:59 +0800

    my webgl utils

--- my-webgl-utils.js
+++ my-webgl-utils.js
@@ -2,19 +2,29 @@
     //browser globals
     root.myWebglUtils = factory.call(root);
 }(this, function () {
-    function createProgramFromScripts() {
-        console.log('hello world');
+    function createProgramFromScripts(gl, selectors, opt_attribs, opt_locations) {
+        var shaders = [];
+        for (var ii = 0; ii < selectors.length; ii++) {
+            shaders.push(createShaderFromScript(gl, selectors[ii], gl[defaultShaderType[ii]]));
+        }
+        return createProgram(gl, shaders, opt_attribs, opt_locations);
     }
+
+    var defaultShaderType = [
+        "VERTEX_SHADER",
+        "FRAGMENT_SHADER",
+    ];
 
     function createShaderFromScript(gl, selector, opt_shaderType) {
         var shaderSource = '';
         var shaderType;
         var shaderScript = document.querySelector(selector);
-        if (!shaderSource) {
-            throw ('*** Error: unknown script element ***' + selercor);
-        }
-        shaderSource = shaderScript.text;
 
+        if (!shaderScript) {
+            throw ('*** Error: unknown script element ***' + selector);
+        }
+
+        shaderSource = shaderScript.text;
         if(!opt_shaderType) {
             if (!opt_shaderType) {
                 if (shaderScript.type === 'x-shader/x-vertex') {
@@ -26,6 +36,11 @@
                 }
             }
         }
+
+        return loadShader(
+            gl,
+            shaderSource,
+            opt_shaderType ? opt_shaderType : shaderType);
     }
 
     function loadShader(gl, shaderSource, shaderType) {
@@ -42,8 +57,30 @@
         return shader;
     }
 
-    function createProgram() {
+    function createProgram(gl, shaders, opt_attribs, opt_locations) {
+        var program = gl.createProgram();
+        shaders.forEach(function (shader) {
+           gl.attachShader(program, shader);
+        });
+        if (opt_attribs) {
+            opt_attribs.forEach(function (attrib, ind) {
+                gl.bindAttribLocation(
+                    program,
+                    opt_locations ? opt_locations[ind] : ind,
+                    attrib);
+            });
+        }
+        gl.linkProgram(program);
 
+        var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (!linked) {
+            var lastError = gl.getProgramInfoLog(program);
+            console.log('*** Error Linking program: ' + lastError);
+
+            gl.deleteProgram(program);
+            return null;
+        }
+        return program;
     }
 
     return {
